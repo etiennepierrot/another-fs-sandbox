52037a20c2e7de4a16644296bec178fa7c46d520
Etienne Pierrot <etienne.pierrot@gmail.com>
2021-09-20 03:04:48 +0200

    refactor parser

--- fsharpsandbox/Parser.fs
+++ fsharpsandbox/Parser.fs
@@ -2,34 +2,70 @@ module Parser
     open System
     open FSharp.Data
 
-    let RunSample = 
-        let msft = CsvFile.Load("/home/tonton/repos/another-fs-sandbox/fsharpsandbox/extract-2021-09-14_22-09-98.csv")
-        let datediff (arr :DateTime[]) =
-           let diff (d1 :DateTime) (d2 :DateTime) = d1.Subtract(d2).TotalMilliseconds |> Math.Abs
-           diff arr.[0] arr.[1]
+    type DataTrace = { 
+        CorrelationId   : string;
+        Timestamp       : DateTime
+    }
 
+    type Stat = { 
+        SizeSample  : int;
+        MinValue    : float;
+        ``95p``     : float;
+        ``99p``     : float;
+        MaxValue    : float;
+        Average     : float;
+        Median      : float;
+    }
+
+    let Parse (correlationIdField :string) (dateField :string) (path :string) =
+        CsvFile.Load(path).Rows 
+        |> Seq.map (fun row -> {
+                    CorrelationId = row.GetColumn correlationIdField;
+                    Timestamp =  row.GetColumn dateField |> DateTime.Parse;
+                    })
+
+    let CalculateStat (traces  :seq<DataTrace>) = 
         let percentile (percentile :float) (values :seq<float>) = 
             values
             |> Seq.sort
             |> Seq.take(int ((percentile * float (values |> Seq.length) ) / 100.0) )
             |> Seq.last
-           
-        let values = 
-            let parseRow (row :CsvRow) = ( row.GetColumn "@Timestamp" |> DateTime.Parse, row.GetColumn "CorrelationId")
-            msft.Rows
-            |> Seq.map parseRow
-            |> Seq.groupBy snd
-            |> Seq.filter(fun row -> row |> snd |> Seq.length = 2)
-            |> Seq.map(fun row -> row |> snd |> Seq.map fst  |> Seq.toArray |> datediff) 
-        
 
-        printfn "size sample    : %d items" (values |> Seq.length )
-        printfn "min            : %f ms" ( values |> Seq.min )
-        printfn "95p            : %f ms" ( values |> percentile 95.0)
-        printfn "99p            : %f ms" ( values |> percentile 99.0)
-        printfn "max            : %f ms" ( values |> Seq.max )
-        printfn "avg            : %f ms" ( values |> Seq.average )
-        printfn "median         : %f ms" ( values |> percentile 50.0 )
+        let ``p95`` = percentile 95.0
+        let ``p99`` = percentile 99.0
+        let median  = percentile 50.0
 
+        let toStat (values :seq<float>) = 
+         {
+            SizeSample  = values |> Seq.length;
+            MinValue    = values |> Seq.min;
+            ``95p``     = values |> ``p95``;
+            ``99p``     = values |> ``p99``;
+            MaxValue    = values |> Seq.max;
+            Average     = values |> Seq.average;
+            Median      = values |> median
+        } 
 
+        let extractLatency (_, datatraces) = 
+            let latency  (d1 :DateTime, d2 :DateTime) = d1.Subtract(d2).TotalMilliseconds |> Math.Abs
+            let getExtremeValues (datatraces) = (datatraces |> Seq.min, datatraces |> Seq.max)
+            datatraces 
+            |> Seq.map( fun dt -> dt.Timestamp) 
+            |> getExtremeValues
+            |> latency
+
+        traces
+        |> Seq.groupBy (fun x -> x.CorrelationId)
+        |> Seq.filter(fun (_, traces) -> traces |> Seq.length = 2)
+        |> Seq.map extractLatency 
+        |> toStat
+
+    let DisplayStat (stat :Stat) = 
+        printfn "size sample    : %d items" stat.SizeSample
+        printfn "min            : %f ms" stat.MinValue
+        printfn "95p            : %f ms" stat.``95p``
+        printfn "99p            : %f ms" stat.``99p``
+        printfn "max            : %f ms" stat.MaxValue
+        printfn "avg            : %f ms" stat.Average
+        printfn "median         : %f ms" stat.Median 
     
--- fsharpsandbox/Program.fs
+++ fsharpsandbox/Program.fs
@@ -1,20 +1,10 @@
-﻿open System
-open FSharp.Data
+﻿open FSharp.Data
 open Parser
-
-
-
 
 [<EntryPoint>]
 let main argv =
-    RunSample
-    // Average : 353 ms
-    // Min : 190 ms
-    // Max : 952 ms
-    // Max 95p : 491 ms
-    
-    //    |> Seq.iter( fun row -> printfn $"Line: (%s{(fst row)}, %f{(snd row)} ms)" )
-//    
-//    for row in msft.Rows do
-//        printfn "Line: (%s, %s)" (row.GetColumn "@Timestamp") (row.GetColumn "CorrelationId")
+    //dotnet run "/home/tonton/repos/another-fs-sandbox/fsharpsandbox/extract-2021-09-14_22-09-98.csv
+    (Parse "CorrelationId" "date" 
+        >> CalculateStat  
+        >> DisplayStat) (argv.[0])
     0 // return an integer exit code
